36ff69346a69304f153987f28b46d67060a5b6a4
kendocross <chenywork@hotmail.com>
2019-01-29 10:48:56 +0800

    ✨

--- domain/protocol/protocol.go
+++ domain/protocol/protocol.go
@@ -3,8 +3,11 @@ package protocol
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/KendoCross/kendocqrs/domain/bus"
+	"github.com/KendoCross/kendocqrs/infrastructure"
+	"github.com/KendoCross/kendocqrs/interfaces"
 
 	"github.com/google/uuid"
 	eh "github.com/looplab/eventhorizon"
@@ -27,107 +30,54 @@ var _ = eh.Aggregate(&ProtocolAggregate{})
 func (a *ProtocolAggregate) HandleCommand(ctx context.Context, cmd eh.Command) error {
 	switch cmd := cmd.(type) {
 	case *SignProtocol:
-		err := fmt.Errorf(cmd.CardName)
+		//命令只需要确定输入参数满足业务校验即可
+		err := a.CheckSign()
 		if err != nil {
 			return err
 		}
+		//实际的业务可以异步进行处理
+		go a.CfrmSign(cmd)
 
-		bus.RaiseEvents(ctx, a.Events(), 0)
 		return nil
 	}
 	return fmt.Errorf("couldn't handle command")
 }
 
-func (p *ProtocolAggregate) Sign() (string, error) {
-
-	// smsContent := &domainser.QuickPaySignSmsContent{
-	// 	BankCode:     p.SignVm.BankCode,
-	// 	ACCOUNT_TYPE: p.SignVm.AccountType,
-	// 	ACCOUNT_NO:   strings.Replace(p.SignVm.BankCardNo, " ", "", -1),
-	// 	ACCOUNT_NAME: p.SignVm.CardName,
-	// 	ACCOUNT_PROP: p.SignVm.AccountProp,
-	// 	ID_TYPE:      p.SignVm.IDCardType,
-	// 	ID:           p.SignVm.IDCardNo,
-	// 	TEL:          p.SignVm.ReservePhone,
-	// 	MERREM:       p.SignVm.Merrem,
-	// 	REMARK:       p.SignVm.Remark,
-	// 	AisleType:    p.SignVm.AisleType,
-	// }
-	// result := domainser.Allinpay.SignGetCode(smsContent)
-	// jsonByte, err := json.Marshal(result)
-	// if err != nil {
-	// 	return "", err
-	// }
-
-	// return string(jsonByte), nil
-
-	return "", nil
+func (a *ProtocolAggregate) CheckSign() error {
+	//校验输入参数是否有效，
+	return nil
 }
 
-func (p *ProtocolAggregate) CfrmSign(reqSn string, verCode string) (string, error) {
+func (a *ProtocolAggregate) CfrmSign(cmd *SignProtocol) error {
 
-	// result := domainser.ApiResp{}
+	protocolOrm := &interfaces.ProtocolOrm{
+		ProtocolNo:   uuid.New().String(),
+		AisleType:    cmd.AisleType,
+		BankCode:     cmd.BankCode,
+		BankCardNo:   cmd.BankCardNo,
+		ReservePhone: cmd.ReservePhone,
+		CardName:     cmd.CardName,
+		IDCardNo:     cmd.IDCardNo,
+		Merrem:       cmd.Merrem,
+		Remark:       cmd.Remark,
+		Status:       1,
+	}
+	protocolOrm.AccountType, _ = strconv.Atoi(cmd.AccountType)
+	protocolOrm.AccountProp, _ = strconv.Atoi(cmd.AccountProp)
+	protocolOrm.IDCardType, _ = strconv.Atoi(cmd.IDCardType)
 
-	// if !infra.MemoryCache.IsExist(reqSn) {
-	// 	result.ErrMsg = "无此请求号的记录！"
-	// 	jsonByte, err := json.Marshal(result)
-	// 	if err != nil {
-	// 		return "", err
-	// 	}
-	// 	return string(jsonByte), nil
-	// }
+	//这里本应该的业务逻辑请求，通过领域服务
+	//result := domainser.Allinpay.SignGetCode(protocolOrm)
 
-	// smsContent := infra.MemoryCache.Get(reqSn).(domainser.QuickPaySignSmsContent)
-	// smsCfrm := &domainser.SmsCfrmContent{
-	// 	VERCODE:  verCode,
-	// 	SRCREQSN: reqSn,
-	// }
+	protocolRepo := infrastructure.RepoFac.ProtocolRepo
+	_, err := protocolRepo.Add(protocolOrm)
 
-	// result = domainser.Allinpay.SignCfrm(smsCfrm)
-	// if result.IsSuccess {
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+	//业务处理成功后，激活领域事件
+	bus.RaiseEvents(ctx, a.Events(), 0)
 
-	// 	accountType, _ := strconv.Atoi(smsContent.ACCOUNT_TYPE)
-	// 	accountProp, _ := strconv.Atoi(smsContent.ACCOUNT_PROP)
-	// 	idTp, _ := strconv.Atoi(smsContent.ID_TYPE)
-
-	// 	protocolOrm := &ddd_interfaces.ProtocolOrm{
-	// 		ProtocolNo:    uuid.New().String(),
-	// 		AisleType:     smsContent.AisleType,
-	// 		AisleProtocol: result.RetInfo,
-	// 		BankCode:      smsContent.BankCode,
-	// 		AccountType:   accountType,
-	// 		AccountProp:   accountProp,
-	// 		BankCardNo:    smsContent.ACCOUNT_NO,
-	// 		ReservePhone:  smsContent.TEL,
-	// 		IDCardType:    idTp,
-	// 		CardName:      smsContent.ACCOUNT_NAME,
-	// 		IDCardNo:      smsContent.ID,
-	// 		Merrem:        smsContent.MERREM,
-	// 		Remark:        smsContent.REMARK,
-	// 		Status:        1,
-	// 	}
-
-	// 	protocolRepo := crossutting.RepoFac.GetProtocolRepo()
-	// 	_, err := protocolRepo.Add(protocolOrm)
-
-	// 	if err != nil {
-	// 		result.ErrMsg = "保存数据库失败，严重异常！"
-	// 	}
-
-	// 	result.RetInfo = protocolOrm.ProtocolNo
-	// }
-
-	// jsonByte, err := json.Marshal(result)
-	// if err != nil {
-	// 	return "", err
-	// }
-
-	// return string(jsonByte), nil
-
-	// eventEnvelop, err := dddcore.NewEvent(p.id, fmt.Sprintf("%T", p), p.version, protocolOrm)
-	// if err != nil {
-	// 	return "-1", err
-	// }
-	// p.changes = append(p.changes, eventEnvelop)
-	return "", nil
+	return nil
 }
